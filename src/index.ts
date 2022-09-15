05041752e5e6db9144e1bfc0a977f5f501c263ac
great-park <cksgh1735@naver.com>
2022-09-15 21:59:54 +0900

    add simple health check api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import express from "express";
-const app = express(),
+import { exp as express } from "../config/express";
+import { logger } from "../config/winston";
+let port: number;
+if (process.env.NODE_ENV === "development") {
+  port = 3001;
+} else if (process.env.NODE_ENV === "production") {
   port = 3000;
+} else {
+  port = 8080;
+}
 
-app
-  .get("/", (req, res) => res.json({ message: "success" }))
-  .listen(port, () => console.log(`server is listening at ${port}`));
+express().listen(port);
+logger.info(
+  `environment : ${process.env.NODE_ENV} - API Server Start At Port ${port}`
+);
