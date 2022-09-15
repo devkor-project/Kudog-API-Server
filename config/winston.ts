8b98770fad3697cebcf227fb450d350e0f7307d4
great-park <cksgh1735@naver.com>
2022-09-15 21:35:10 +0900

    add winston

---
+++ config/winston.ts
@@ -0,0 +1,43 @@
+import { createLogger, format, transports } from "winston";
+import "winston-daily-rotate-file";
+import * as fs from "fs";
+
+const env = process.env.NODE_ENV || "development";
+const logDir = "logs";
+
+// https://lovemewithoutall.github.io/it/winston-example/
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const dailyRotateFileTransport = new transports.DailyRotateFile({
+  level: "debug",
+  filename: `${logDir}/%DATE%-smart-push.log`,
+  datePattern: "YYYY-MM-DD",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
+
+export const logger = createLogger({
+  level: env === "development" ? "debug" : "info",
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss",
+    }),
+    format.json()
+  ),
+  transports: [
+    new transports.Console({
+      level: "info",
+      format: format.combine(
+        format.colorize(),
+        format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      ),
+    }),
+    dailyRotateFileTransport,
+  ],
+});
