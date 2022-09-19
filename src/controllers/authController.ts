520dcd83cb564b862e8026406cec6a899072f557
great-park <cksgh1735@naver.com>
2022-09-19 22:00:38 +0900

    [feat] login API

---
+++ src/controllers/authController.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import { response, errResponse } from "../interfaces/response/response";
+import { message } from "../interfaces/response/responseMessage";
+import * as authService from "../services/authService";
+import JWT from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import { prisma } from "../utils/util";
+dotenv.config();
+
+/**
+ * 로그인 API
+ * @method post
+ * @url /auth/login
+ * @body email, password
+ * @return_data accessToken
+ */
+export const login = async function (req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  // Look for user email in the database
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  // If user not found, send error message
+  if (!user) {
+    return res.status(400).send(errResponse(message.SIGNIN_EMAIL_NOT_EXISTS));
+  }
+
+  //   // Compare hased password with user password to see if they are valid
+  //   const isMatch = await bcrypt.compare(password, user.password);
+
+  //   if (!isMatch) {
+  //     return res.status(401).send(errResponse(message.SIGNIN_INVALID_PASSWORD));
+  //   }
+
+  //login 성공 result
+  const loginResult = await authService.login(email);
+
+  res.send(response(message.SUCCESS, loginResult));
+};
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,4 +1,2 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-
-module.exports = { prisma };
+export const prisma = new PrismaClient();
