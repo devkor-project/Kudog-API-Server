b486a161b5d0e92be3a1abbc062bd0c856915b58
great-park <cksgh1735@naver.com>
2022-09-21 19:22:57 +0900

    [remove] clear

--- src/dtos/user.dto.ts
+++
@@ -1,9 +0,0 @@
-import * as validator from "class-validator";
-
-export class logInUserDto {
-  @validator.IsEmail()
-  public email: string;
-
-  @validator.IsString()
-  public password: string;
-}
