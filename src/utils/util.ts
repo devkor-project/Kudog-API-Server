65b417b2e0adba7b483a5c86c1da426567766a7b
great-park <cksgh1735@naver.com>
2022-09-21 19:20:18 +0900

    [remove] clear

--- src/utils/util.ts
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-<<<<<<< HEAD
-const prisma = new PrismaClient();
-
-module.exports = { prisma };
-=======
-export const prisma = new PrismaClient();
->>>>>>> parent of c16bb69 ([edit] 구조 변경중)
