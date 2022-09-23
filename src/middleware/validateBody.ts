c7800b4193575438610ec2ad4eedfea25a27edf9
overthestream <njhnjh02@naver.com>
2022-09-23 14:12:22 +0900

    fix: add try-catch for async function

--- src/middleware/validateBody.ts
+++ src/middleware/validateBody.ts
@@ -8,23 +8,23 @@ export const loginValidation = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const schema = Joi.object({
-    // check e-mail
-    email: Joi.string().email(),
-
-    // length : 8 ~ 64, ASCII 테이블에 있는 문자만 입력 가능
-    password: Joi.string()
-      .min(8)
-      .max(64)
-      .regex(/^[ -~]+$/i),
-  });
-
-  // validate req.body
   try {
-    await schema.validateAsync(req.body);
+    const schema = Joi.object({
+      // check e-mail
+      email: Joi.string().email(),
+
+      // length : 8 ~ 64, ASCII 테이블에 있는 문자만 입력 가능
+      password: Joi.string()
+        .min(8)
+        .max(64)
+        .regex(/^[ -~]+$/i),
+    });
+
+    // validate req.body
+    const result = await schema.validateAsync(req.body);
+    if (result.err) throw INVALID_FORMAT;
+    next();
   } catch (err) {
-    // throw INVALID_FORMAT; 서버 다운됨
-    return res.status(400).send('수정 필요');
+    next(err);
   }
-  next();
 };
