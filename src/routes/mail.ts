1e116023bf9846bf4ca09238bdc9f49c93998a00
great-park <cksgh1735@naver.com>
2022-11-16 16:30:24 +0900

    remove: mail API

--- src/routes/mail.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as mail from '@/controllers/mailController';
-
-const router = express.Router();
-
-router.post('/send', mail.sendMail);
-
-export default router;
